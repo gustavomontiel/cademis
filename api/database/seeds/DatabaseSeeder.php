e5ead1b1d5d5349bd8683b09a8ecbc29c469f876
Fernando Chamorro <joseferchamorro@gmail.com>
2019-12-15 20:29:10 -0300

    API - Primera versión de la api con Autentificación y CRUD de usuarios

---
+++ api/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            RolesSeeder::class,
+        ]);
+        User::createFromValues('Administrador', 'admin', 'admin@cademis.com', '123456')->assignRole('administrador');
+        User::createFromValues('Usuario', 'user', 'user@cademis.com', '123456');
+    }
+}
---
+++ api/routes/api.php
@@ -0,0 +1,83 @@
+<?php
+/** @var Router $router */
+
+use Laravel\Lumen\Routing\Router;
+
+/* Public Routes */
+$router->get('/', function () {
+    return response()->json(['message' => 'Welcome to Lumen API Starter']);
+});
+
+/* Auth Routes */
+$router->group(['prefix' => 'auth', 'as' => 'auth'], function (Router $router) {
+
+    /* Defaults */
+    $router->post('/register', [
+        'as' => 'register',
+        'uses' => 'AuthController@register',
+    ]);
+    $router->post('/login', [
+        'as' => 'login',
+        'uses' => 'AuthController@login',
+    ]);
+    $router->get('/verify/{token}', [
+        'as' => 'verify',
+        'uses' => 'AuthController@verify'
+    ]);
+
+    /* Password Reset */
+    $router->post('/password/forgot', [
+        'as' => 'password.forgot',
+        'uses' => 'AuthController@forgotPassword'
+    ]);
+    $router->post('/password/recover/{token}', [
+        'as' => 'password.recover',
+        'uses' => 'AuthController@recoverPassword'
+    ]);
+
+    /* Protected User Endpoint */
+    $router->get('/user', [
+        'uses' => 'AuthController@getUser',
+        'as' => 'user',
+        'middleware' => 'auth'
+    ]);
+});
+
+/* Protected Routes */
+$router->group(['middleware' => 'auth'], function (Router $router) {
+
+    $router->group(['middleware' => 'role:administrador'], function (Router $router) {
+
+        $router->get('/admin', function () {
+            return response()->json(['message' => 'Eres un administrador.']);
+        });
+
+        /* Users Routes */
+        $router->get('/users', [
+            'as' => 'users.index',
+            'uses' => 'UserController@index'
+        ]);
+
+        $router->get('/users/{id}', [
+            'as' => 'users.show',
+            'uses' => 'UserController@show'
+        ]);
+
+        $router->post('/users', [
+            'as' => 'users.store',
+            'uses' => 'UserController@store'
+        ]);
+
+        $router->put('/users/{id}', [
+            'as' => 'users.update',
+            'uses' => 'UserController@update'
+        ]);
+
+        $router->delete('/users/{id}', [
+            'as' => 'users.destroy',
+            'uses' => 'UserController@destroy'
+        ]);
+
+    });
+
+});
